18fcec5a3825259b143942bee907bee9b16828e7
LOLIPOP1136 <117951436+LOLIPOP1136@users.noreply.github.com>
2023-01-06 22:40:38 +0100

    Fixing a mistake in the changes

---
+++ MauiProgram.cs
@@ -0,0 +1,42 @@
+using BusApplication.Repositories;
+using BusApplication.ViewModel;
+using Pages;
+
+namespace BusApplication;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+		string dbPath = FileAccessHelper.GetLocalFilePath("people.db3");
+        builder.Services.AddSingleton<DriverRepository>(s => ActivatorUtilities.CreateInstance<DriverRepository>(s, dbPath));
+
+		string dbPathBuses = FileAccessHelper.GetLocalFilePath("buses.db3");
+        builder.Services.AddSingleton<BusRepository>(s => ActivatorUtilities.CreateInstance<BusRepository>(s, dbPathBuses));
+
+        builder.Services.AddSingleton<MainPage>();
+		builder.Services.AddSingleton<MainViewModel>();
+
+		builder.Services.AddTransient<DriverManagementPage>();
+		builder.Services.AddTransient<DriverManagementViewModel>();
+
+		builder.Services.AddSingleton<FindBusPage>();
+		builder.Services.AddSingleton<FindBusViewModel>();
+
+		builder.Services.AddSingleton<RouteDetailsPage>();
+		/*
+		 *Transient is created and destroyed each time, useful for dynamic content
+		 *Singleton is created once and not destroyed
+		 */
+        return builder.Build();
+	}
+}
