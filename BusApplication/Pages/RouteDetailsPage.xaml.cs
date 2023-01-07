c6ed7c0889045ff9131911581a7f4623ee940b66
LOLIPOP1136 <117951436+LOLIPOP1136@users.noreply.github.com>
2023-01-08 00:36:32 +0100

    Update RouteDetailsPage.xaml.cs

--- BusApplication/Pages/RouteDetailsPage.xaml.cs
+++ BusApplication/Pages/RouteDetailsPage.xaml.cs
@@ -1,9 +1,18 @@
+using BusApplication.ViewModel;
+
 namespace Pages;
 
 public partial class RouteDetailsPage : ContentPage
 {
-	public RouteDetailsPage()
-	{
-		InitializeComponent();
-	}
+    public RouteDetailsPage(RouteDetailsViewModel vm)
+    {
+        InitializeComponent();
+        BindingContext = vm;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+    }
 }
