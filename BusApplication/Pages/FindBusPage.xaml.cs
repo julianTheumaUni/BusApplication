779a9756d8e386ac4eb3b453cfa6ae9e64550093
LOLIPOP1136 <117951436+LOLIPOP1136@users.noreply.github.com>
2023-01-07 14:37:43 +0100

    Update FindBusPage.xaml.cs

--- BusApplication/Pages/FindBusPage.xaml.cs
+++ BusApplication/Pages/FindBusPage.xaml.cs
@@ -1,12 +1,18 @@
 using BusApplication.ViewModel;
 
-namespace Pages;
+namespace BusApplication;
 
 public partial class FindBusPage : ContentPage
 {
-	public FindBusPage(FindBusViewModel vm)
-	{
-		InitializeComponent();
-		BindingContext = vm;
-	}
+    public FindBusPage(FindBusViewModel vm)
+    {
+        InitializeComponent();
+        BindingContext = vm;
+    }
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+    }
 }
